8c89b308f284e33f4a81c48fe26e03a798fc11b3
RyoYamada <newfield1001@gmail.com>
2020-09-28 18:08:39 +0900

    add fragment

--- app/src/main/java/com/ryoyamada/agentodo/MainActivity.kt
+++ app/src/main/java/com/ryoyamada/agentodo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ryoyamada.agentodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ryoyamada.agentodo.ui.TodoListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, TodoListFragment())
+            fragmentTransaction.commit()
+        }
     }
 }
