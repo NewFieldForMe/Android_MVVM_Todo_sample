6c71f4b6329c3f9c68b45adc0ed005758bb77f1d
RyoYamada <newfield1001@gmail.com>
2020-09-28 17:44:07 +0900

    add ToDoListFragment

---
+++ app/src/main/java/com/ryoyamada/agentodo/ui/TodoListFragment.kt
@@ -0,0 +1,18 @@
+package com.ryoyamada.agentodo.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ryoyamada.agentodo.R
+
+class TodoListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+}
