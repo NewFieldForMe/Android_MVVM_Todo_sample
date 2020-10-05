a0f8b3853f9a5201f8632a1bf7c72814ef54bf40
RyoYamada <newfield1001@gmail.com>
2020-10-05 18:50:12 +0900

    add recyclerview in fragment

--- app/src/main/java/com/ryoyamada/agentodo/ui/TodoListFragment.kt
+++ app/src/main/java/com/ryoyamada/agentodo/ui/TodoListFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ryoyamada.agentodo.R
+import com.ryoyamada.agentodo.model.Todo
+import kotlinx.android.synthetic.main.fragment_todo_list.*
+import java.util.*
 
 class TodoListFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +19,18 @@ class TodoListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = TodoListAdapter()
+        val todo1 = Todo(UUID.randomUUID(), "test1")
+        val todo2 = Todo(UUID.randomUUID(), "test2")
+        val todo3 = Todo(UUID.randomUUID(), "test3")
+        adapter.todos = listOf(todo1, todo2, todo3)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
     }
 }
