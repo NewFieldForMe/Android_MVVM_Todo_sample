362b0d50b436659c2d37aae0bad3557c591f0f98
RyoYamada <newfield1001@gmail.com>
2020-10-14 16:46:48 +0900

    pop back from input todo fragment when tap fab

--- app/src/main/java/com/ryoyamada/agentodo/ui/InputTodoFragment.kt
+++ app/src/main/java/com/ryoyamada/agentodo/ui/InputTodoFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ryoyamada.agentodo.R
+import kotlinx.android.synthetic.main.fragment_input_todo.*
 
 class InputTodoFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +15,12 @@ class InputTodoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_input_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            this.fragmentManager?.popBackStack()
+        }
     }
 }
